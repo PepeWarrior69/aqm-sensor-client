13137f64c362ac23f2bdd8a1320f4a49cefbbc19
Alex <alexbabulis.dev@gmail.com>
2023-11-18 22:56:01 +0200

    fixed env var name

--- config/config.py
+++ config/config.py
@@ -5,6 +5,6 @@ import os
 load_dotenv()
 
 # Access environment variables
-SENSOR_READ_FREYENCY_SEC = int(os.getenv("SENSOR_READ_FREYENCY_SEC")) if os.getenv("SENSOR_READ_FREYENCY_SEC") else 1
+SENSOR_READ_FREYENCY_SEC = int(os.getenv("SENSOR_READ_FREQUENCY_SEC")) if os.getenv("SENSOR_READ_FREYENCY_SEC") else 1
 SEND_DATA_FREQUENCY_SEC = int(os.getenv("SEND_DATA_FREQUENCY_SEC")) if os.getenv("SEND_DATA_FREQUENCY_SEC") else 10
 ENDPOINT_URL = os.getenv("ENDPOINT_URL")
